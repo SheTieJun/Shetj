4315544977037e446f6ee05af1753483b7c74fd8
shetj <375105540@qq.com>
2018-08-29 16:25:46 +0800

    配置aspect

---
+++ aspectutils/src/main/java/me/shetj/aspectutils/PermissionAspect.java
@@ -0,0 +1,42 @@
+package me.shetj.aspectutils;
+
+import android.app.Activity;
+
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+import io.reactivex.functions.Consumer;
+import me.shetj.base.tools.app.ArmsUtils;
+
+@Aspect
+public class PermissionAspect {
+	@Pointcut("execution(@me.shetj.aspectutils.MPermission * *(..)) && @annotation(permission)")
+	public void methodAnnotatedWithMPermission(MPermission permission) {}
+
+	@Around("methodAnnotatedWithMPermission(permission)")
+	public void checkPermission(final ProceedingJoinPoint joinPoint, MPermission permission) throws Throwable {
+		String[] permissionStr = permission.value();
+		if (joinPoint.getThis() instanceof Activity){
+			RxPermissions	rxPermissions = new RxPermissions((Activity) joinPoint.getThis());
+			rxPermissions.request(permissionStr)
+							.subscribe(new Consumer<Boolean>() {
+								@Override
+								public void accept(Boolean aBoolean) throws Exception {
+									if (aBoolean){
+										try {
+											joinPoint.proceed();
+										} catch (Throwable throwable) {
+											throwable.printStackTrace();
+										}
+									}else {
+										ArmsUtils.makeText("没有对应权限，无法使用该功能~！");
+									}
+								}
+							});
+		}
+	}
+}
