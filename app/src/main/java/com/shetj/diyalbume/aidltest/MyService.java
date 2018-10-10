80fc70d60ca3bb9d2bd9338a62a6158994c5ea54
shetj <375105540@qq.com>
2018-10-10 16:22:04 +0800

    test aidl 打开APP

--- app/src/main/java/com/shetj/diyalbume/aidltest/MyService.java
+++ app/src/main/java/com/shetj/diyalbume/aidltest/MyService.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.IBinder;
 
 import com.shetj.diyalbume.IMyAidlInterface;
+import com.shetj.diyalbume.main.view.Main3Activity;
 
 public class MyService extends Service {
 	public MyService() {
@@ -20,6 +21,7 @@ public class MyService extends Service {
 
 		@Override
 		public String getName() {
+			startActivity(new Intent(getBaseContext(),Main3Activity.class));
 			return "AIDL-test：getPackageName:"+getPackageName();
 		}
 	}
