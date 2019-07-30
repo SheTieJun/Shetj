da53e7cfca9157869826994e5e6fa9020ce3e27f
shetj <375105540@qq.com>
2019-07-30 23:46:06 +0800

    测试 layoutmanger

---
+++ app/src/main/java/com/shetj/diyalbume/swipcard/ImageAdapter.kt
@@ -0,0 +1,21 @@
+package com.shetj.diyalbume.swipcard
+
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.BaseViewHolder
+import com.shetj.diyalbume.R
+
+/**
+ *
+ * <b>@author：</b> shetj<br>
+ * <b>@createTime：</b> 2019/7/30<br>
+ * <b>@email：</b> 375105540@qq.com<br>
+ * <b>@describe</b>  <br>
+ */
+class ImageAdapter(data:ArrayList<String>) :BaseQuickAdapter<String,BaseViewHolder>(R.layout.item_detail,data){
+
+
+    override fun convert(helper: BaseViewHolder?, item: String?) {
+
+    }
+
+}
