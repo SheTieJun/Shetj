d0f9ca75ee5c3655f61afc74e2637f70c9614002
Jun <375105540@qq.com>
2019-12-16 17:13:04 +0800

    [add] MotionLayout test

--- constraintlayout/src/main/java/me/shetj/constraintlayout/demo/MotionDrawerLayoutActivity.kt
+++ constraintlayout/src/main/java/me/shetj/constraintlayout/demo/MotionDrawerLayoutActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import me.shetj.constraintlayout.R
 
-class DrawerLayoutActivity : AppCompatActivity() {
+class MotionDrawerLayoutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- constraintlayout/src/main/java/me/shetj/constraintlayout/demo/MotionLayoutActivity.kt
+++ constraintlayout/src/main/java/me/shetj/constraintlayout/demo/MotionLayoutActivity.kt
@@ -2,9 +2,12 @@ package me.shetj.constraintlayout.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout.*
 import kotlinx.android.synthetic.main.activity_motion_layout.*
+import kotlinx.android.synthetic.main.motion_basic_01.*
 import me.shetj.base.kt.start
 import me.shetj.constraintlayout.R
+import me.shetj.constraintlayout.demo.youtubedemo.YouTubeDemoActivity
 
 class MotionLayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,11 +19,19 @@ class MotionLayoutActivity : AppCompatActivity() {
         }
 
         btn_DrawerLayout.setOnClickListener{
-            start(DrawerLayoutActivity::class.java)
+            start(MotionDrawerLayoutActivity::class.java)
         }
 
         btn_fragment.setOnClickListener {
-            start(FragmentMotionActivity::class.java)
+            start(MotionFragmentActivity::class.java)
+        }
+        //    public static final int DEBUG_SHOW_NONE = 0;
+        //    public static final int DEBUG_SHOW_PROGRESS = 1;
+        //    public static final int DEBUG_SHOW_PATH = 2;
+        motion_1.setDebugMode(DEBUG_SHOW_PATH)
+
+        btn_youTu.setOnClickListener {
+            start(YouTubeDemoActivity::class.java)
         }
     }
 }
